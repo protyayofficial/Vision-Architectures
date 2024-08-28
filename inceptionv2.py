43ef37a0d081e00c4e1e25430d180bd93980c43f
protyayofficial <47077533+protyayofficial@users.noreply.github.com>
2024-08-28 23:45:26 +0530

    Added inceptionv2

--- inceptionv2.py
+++ inceptionv2.py
@@ -45,86 +45,135 @@ class AuxiliaryClassifier(nn.Module):
 
         return x
     
-class InceptionBlock(nn.Module):
-    """
-    Inception Block used within the GoogLeNet architecture.
-    Comprises multiple convolutional paths with varying kernel sizes, enabling the model to capture
-    multi-scale features.
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
+        super().__init__()
+        
+        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
+        self.bn = nn.BatchNorm2d(num_features=out_channels)
+        self.relu = nn.ReLU(in_place=True)
 
-    Args:
-        in_channels (int): Number of input channels.
-        num_conv_1 (int): Number of output channels for the 1x1 convolution.
-        num_conv_3_reduce (int): Number of output channels for the 1x1 convolution preceding the 3x3 convolution.
-        num_conv_3 (int): Number of output channels for the 3x3 convolution.
-        num_conv_5_reduce (int): Number of output channels for the 1x1 convolution preceding the 5x5 convolution.
-        num_conv_5 (int): Number of output channels for the 5x5 convolution.
-        num_pool_proj (int): Number of output channels for the 1x1 convolution applied after max pooling.
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
 
-    Methods:
-        forward(x):
-            Performs the forward pass through the InceptionBlock.
-    """
+        return x
     
+class InceptionF5(nn.Module):  
     def __init__(self, in_channels, num_conv_1, num_conv_3_reduce, num_conv_3, num_conv_5_reduce, num_conv_5, num_pool_proj):
         super().__init__()
 
-        self.conv_1 = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=num_conv_1, kernel_size=1),
-            nn.ReLU()
+        self.block_1 = ConvBlock(in_channels=in_channels, out_channels=num_conv_1, kernel_size=1, stride=1, padding=0)
+        
+        self.block_1_3 = nn.Sequential(
+            ConvBlock(in_channels=in_channels, out_channels=num_conv_3_reduce, kernel_size=1, stride=1, padding=0),
+            ConvBlock(in_channels=num_conv_3_reduce, out_channels=num_conv_3, kernel_size=3, stride=1, padding=1)
         )
 
-        self.conv_3_reduce = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=num_conv_3_reduce, kernel_size=1),
-            nn.ReLU()
-        )
+        self.block_pool_1 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            ConvBlock(in_channels=in_channels, out_channels=num_pool_proj, kernel_size=3, stride=1, padding=1)
+        )       
 
-        self.conv_5_reduce = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=num_conv_5_reduce, kernel_size=1),
-            nn.ReLU()
-        )
-
-        self.pool_proj = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=num_pool_proj, kernel_size=1),
-            nn.ReLU()
-        )
-
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
-
-        self.conv_3 = nn.Sequential(
-            nn.Conv2d(in_channels=num_conv_3_reduce, out_channels=num_conv_3, kernel_size=3, padding=1),
-            nn.ReLU()
-        )
-
-        self.conv_5 = nn.Sequential(
-            nn.Conv2d(in_channels=num_conv_5_reduce, out_channels=num_conv_5, kernel_size=5, padding=2),
-            nn.ReLU()
+        self.block_1_3_3 =  nn.Sequential(
+            ConvBlock(in_channels=in_channels, out_channels=num_conv_5_reduce, kernel_size=1, stride=1, padding=0),
+            ConvBlock(in_channels=num_conv_5_reduce, out_channels=num_conv_5, kernel_size=3, stride=1, padding=1),
+            ConvBlock(in_channels=num_conv_5, out_channels=num_conv_5, kernel_size=3, stride=1, padding=1),
+            
+            
         )
 
     def forward(self, x):
-        """
-        Forward pass through the InceptionBlock.
+        out_1 = self.block_1(x)
+        out_2 = self.block_pool_1(x)
+        out_3 = self.block_1_3(x)
+        out_4 = self.block_1_3_3(x)
 
-        Args:
-            x (torch.Tensor): Input tensor of shape (batch_size, in_channels, height, width).
 
-        Returns:
-            torch.Tensor: Concatenated output from all paths of the Inception block.
-        """
-        out_1 = self.conv_1(x)
-        out_2 = self.conv_3_reduce(x)
-        out_2 = self.conv_3(out_2)
-        out_3 = self.conv_5_reduce(x)
-        out_3 = self.conv_5(out_3)
-        out_4 = self.maxpool(x)
-        out_4 = self.pool_proj(out_4)
+        x = torch.cat([out_1, out_2, out_3, out_4], dim=1)
+
+        return x
+    
+class InceptionF6(nn.Module):  
+    def __init__(self, in_channels, num_conv_1, num_conv_7_small, num_conv_7_small_out, num_conv_7_large, num_conv_7_large_out, num_pool_proj):
+        super().__init__()
+
+        self.block_1 = ConvBlock(in_channels=in_channels, out_channels=num_conv_1, kernel_size=1, stride=1, padding=0)
+
+        self.block_pool_1 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            ConvBlock(in_channels=in_channels, out_channels=num_pool_proj, kernel_size=3, stride=1, padding=1)
+        )   
+        
+        self.block_1_3_small_1 = nn.Sequential(
+            ConvBlock(in_channels=in_channels, out_channels=num_conv_7_small, kernel_size=1, stride=1, padding=0),
+            ConvBlock(in_channels=num_conv_7_small, out_channels=num_conv_7_small, kernel_size=(1, 7), stride=1, padding=(0, 3)),
+            ConvBlock(in_channels=num_conv_7_small, out_channels=num_conv_7_small_out, kernel_size=(7, 1), stride=1, padding=(3, 0)),
+        )
+
+            
+
+        self.block_1_3_large =  nn.Sequential(
+            ConvBlock(in_channels=in_channels, out_channels=num_conv_7_large, kernel_size=1, stride=1, padding=0),
+            ConvBlock(in_channels=num_conv_7_large, out_channels=num_conv_7_large, kernel_size=(1, 7), stride=1, padding=(0, 3)),
+            ConvBlock(in_channels=num_conv_7_large, out_channels=num_conv_7_large, kernel_size=(7, 1), stride=1, padding=(3, 0)),
+            ConvBlock(in_channels=num_conv_7_large, out_channels=num_conv_7_large, kernel_size=(1, 7), stride=1, padding=(0, 3)),
+            ConvBlock(in_channels=num_conv_7_large, out_channels=num_conv_7_large_out, kernel_size=(7, 1), stride=1, padding=(3, 0)),            
+        )
+
+    def forward(self, x):
+        out_1 = self.block_1(x)
+        out_2 = self.block_pool_1(x)
+        out_3 = self.block_1_7_small(x)
+        out_4 = self.block_1_7_large(x)
+
+
+        x = torch.cat([out_1, out_2, out_3, out_4], dim=1)
+
+        return x
+    
+class InceptionF7(nn.Module):  
+    def __init__(self, in_channels, num_conv_1, num_conv_7_small, num_conv_7_small_out, num_conv_7_large, num_conv_7_large_out, num_pool_proj):
+        super().__init__()
+
+        self.block_1 = ConvBlock(in_channels=in_channels, out_channels=num_conv_1, kernel_size=1, stride=1, padding=0)
+
+        self.block_pool_1 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            ConvBlock(in_channels=in_channels, out_channels=num_pool_proj, kernel_size=3, stride=1, padding=1)
+        )   
+        
+        self.block_1_7_small = nn.Sequential(
+            ConvBlock(in_channels=in_channels, out_channels=num_conv_7_small, kernel_size=1, stride=1, padding=0),
+            ConvBlock(in_channels=num_conv_7_small, out_channels=num_conv_7_small, kernel_size=(1, 7), stride=1, padding=(0, 3)),
+            ConvBlock(in_channels=num_conv_7_small, out_channels=num_conv_7_small_out, kernel_size=(7, 1), stride=1, padding=(3, 0)),
+        )
+
+            
+
+        self.block_1_7_large =  nn.Sequential(
+            ConvBlock(in_channels=in_channels, out_channels=num_conv_7_large, kernel_size=1, stride=1, padding=0),
+            ConvBlock(in_channels=num_conv_7_large, out_channels=num_conv_7_large, kernel_size=(1, 7), stride=1, padding=(0, 3)),
+            ConvBlock(in_channels=num_conv_7_large, out_channels=num_conv_7_large, kernel_size=(7, 1), stride=1, padding=(3, 0)),
+            ConvBlock(in_channels=num_conv_7_large, out_channels=num_conv_7_large, kernel_size=(1, 7), stride=1, padding=(0, 3)),
+            ConvBlock(in_channels=num_conv_7_large, out_channels=num_conv_7_large_out, kernel_size=(7, 1), stride=1, padding=(3, 0)),            
+        )
+
+    def forward(self, x):
+        out_1 = self.block_1(x)
+        out_2 = self.block_pool_1(x)
+        out_3 = self.block_1_7_small(x)
+        out_4 = self.block_1_7_large(x)
+
 
         x = torch.cat([out_1, out_2, out_3, out_4], dim=1)
 
         return x
 
-class GoogLeNet(nn.Module):
+class InceptionV2(nn.Module):
     """
-    GoogLeNet (Inception v1) architecture implementation in PyTorch.
+    InceptionV2 architecture implementation in PyTorch.
 
     This implementation includes auxiliary classifiers as per the original 
     paper to help with convergence during training. The auxiliary classifiers 
@@ -179,7 +228,7 @@ class GoogLeNet(nn.Module):
     
     def __init__(self, aux_classifiers=True, num_classes=1000):
         """
-        Initializes the GoogLeNet model with the option to use auxiliary classifiers.
+        Initializes the InceptionV2 model with the option to use auxiliary classifiers.
 
         Parameters:
         -----------
@@ -326,8 +375,8 @@ class GoogLeNet(nn.Module):
 if __name__ == "__main__":
     from torchsummary import summary
 
-    # Instantiate the GoogLeNet model and move it to the GPU
-    model = GoogLeNet().to('cuda')
+    # Instantiate the InceptionV2 model and move it to the GPU
+    model = InceptionV2().to('cuda')
 
     # Print a summary of the model architecture
     print(summary(model, (3, 224, 224)))
